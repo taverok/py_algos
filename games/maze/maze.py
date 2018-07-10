17afd94713b1cf96aa1084501063f65c79de3cb6
Denis T <tr.denis@gmail.com>
2018-07-10 19:13:02 +0300

    maze

--- games/maze/maze.py
+++ games/maze/maze.py
@@ -6,29 +6,32 @@ class Maze:
 
     def __init__(self, arr):
         self.arr = arr
-        self.max_raw = len(arr)-1
+        self.max_row = len(arr)-1
         self.max_col = len(arr[0])-1
-        self.exit_loc = self.max_raw, self.max_col
-        self.__visited = []
+        self.exit_loc = self.max_row, self.max_col
+        self.visited = []
     
+    def should_visit(self, row, col):
+        return (row, col) not in self.visited and 0<=row<=self.max_row and 0<=col<=self.max_col
+
     def has_path_recursive(self, row, col):
+        self.visited.append((row, col))
+
         if self.exit_loc == (row, col):
             print('{} {} exit'.format(row, col))
             return True
 
         if Maze.WALL == self.arr[row][col]:
             print('{} {} wall'.format(row, col))
-            self.__visited.append((row, col))
             return False
         
-        for next_row in range(row-1, row+1):
-            for next_col in range(col-1, col+1):
-                if row <= self.max_raw \
-                    and next_col <= self.max_col \
-                    and (next_row, next_col) not in self.__visited :
-                    print('{} {} visiting'.format(row, col))
-
-                    return self.__visited.append((next_row, next_col))
+        if (self.should_visit(row-1, col) and self.has_path_recursive(row-1, col))\
+            or (self.should_visit(row+1, col) and self.has_path_recursive(row+1, col))\
+            or (self.should_visit(row, col-1) and self.has_path_recursive(row, col-1))\
+            or (self.should_visit(row, col+1) and self.has_path_recursive(row, col+1)):
+            return True
+        
+        return False
 
 
 
@@ -37,9 +40,8 @@ class Test(unittest.TestCase):
         maze = Maze([
             ['.','W','.'],
             ['.','W','.'],
-            ['.','W','.'],
+            ['.','.','.'],
         ])
-        self.assertFalse(maze.has_path_recursive(0,0))
         self.assertTrue(maze.has_path_recursive(0,0))
 
 if __name__ == '__main__':
